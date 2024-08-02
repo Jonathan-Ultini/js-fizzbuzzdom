51be7a972f4178995eb1ad3ac6e5ba8167bf8bd7
Jonathan-Ultini <jonathanultini@gmail.com>
2024-08-02 15:11:23 +0200

    milestone 3

--- js/script.js
+++ js/script.js
@@ -5,20 +5,26 @@ const container = document.getElementById('fizzbuzz-container');
 
 // stampiamo a console 100 numeri, con risultati diversi in base alle richieste
 for (let i = 1; i <= 100; i++) {
+
   let output = '';
+  let className = 'fizzbuzz-item';
+
   if (i % 3 === 0 && i % 5 === 0) {
     output = 'FizzBuzz';
+    className += ' fizzbuzz';
   } else if (i % 3 === 0) {
     output = 'Fizz';
+    className += ' fizz';
   } else if (i % 5 === 0) {
     output = 'Buzz';
+    className += ' buzz';
   } else {
     output = i;
   }
 
   // Creiamo un nuovo elemento div per ogni risultato
   const item = document.createElement('div');
-  item.className = 'fizzbuzz-item';
+  item.className = className;
   item.innerText = output;
 
   // Aggiungi l'elemento al contenitore
